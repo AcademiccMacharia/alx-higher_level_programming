feba1f9681a9f85cd2d7c291d390dc3dc9b7486c
AcademiccMacharia <machariab19@gmail.com>
2022-12-28 00:24:22 +0300

    fixing errors

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,9 +1,6 @@
 -- Script that creates a table in MySQL Server
 -- Query to create the table 'id_not_null'
 CREATE TABLE IF NOT EXISTS id_not_null(
-	id INT,
+	id INT DEFAULT 1,
 	name VARCHAR(256)
-	)
-	VALUES(
-		id = '1'
-		);
+	);
