99b5d3b358a12c61e3de9ff426bbb43d48d2b217
AcademiccMacharia <machariab19@gmail.com>
2022-12-27 20:48:16 +0300

    fixing errors

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Script that lists all priviledges of the MySQL users
 -- Query to list all priviledges (GRANT) of the MySQL users
-SHOW GRANTS FOR 'user_0d_1';
-SHOW GRANTS FOR 'user_0d_2';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
