fc5c60e1f50be4cbcb6446cff2783cf971079f4c
AcademiccMacharia <machariab19@gmail.com>
2022-12-27 20:44:45 +0300

    adding a script that lists priviledges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- Script that lists all priviledges of the MySQL users
 -- Query to list all priviledges (GRANT) of the MySQL users
-SHOW GRANTS FOR 'user_0d_1' AND 'user_0d_2';
+SHOW GRANTS FOR 'user_0d_1';
+SHOW GRANTS FOR 'user_0d_2';
