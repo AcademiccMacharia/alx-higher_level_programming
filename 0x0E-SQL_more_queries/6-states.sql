75469359577c9b02b563cb50ea3848e17f9dea8e
AcademiccMacharia <machariab19@gmail.com>
2022-12-28 00:33:52 +0300

    fixing errors

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Script that creates a database and table
 -- Query to create the database 'hbtn_0d_usa' and the table 'states'
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
 	id INT UNIQUE AUTO INCREMENT NOT NULL PRIMARY KEY,
 	name VARCHAR(256) NOT NULL);
