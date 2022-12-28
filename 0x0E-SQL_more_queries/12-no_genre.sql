d12f1572033a0b5e8f9fa250746b4484938ba841
AcademiccMacharia <machariab19@gmail.com>
2022-12-28 10:22:12 +0300

    fixing errors

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- Script to get records from database
 -- Query to get genres that are not linked
-SELECT tv_shows.title, tv_shows_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
