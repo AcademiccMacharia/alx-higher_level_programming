74ba8e05b3a99e896f65f6adf5641680613cf632
AcademiccMacharia <machariab19@gmail.com>
2023-01-06 11:32:30 +0300

    script that prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const lines = {
+	first: 'C is fun',
+	second: 'Python is cool', 
+	third: 'JavaScript is amazing',
+};
+console.log(lines.first);
+console.log(lines.second);
+console.log(lines.third);
