c356bd30c97cf4dc0fbb10829ab61fe88b052dcc
AcademiccMacharia <machariab19@gmail.com>
2023-01-09 15:57:07 +0300

    fixing errors

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return (list(map(lambda x: list(map(lambda y: y**2, x)), matrix)))
+def complex_delete(a_dictionary, value):
+    keys_to_delete = []
+    for key, val in a_dictionary.items():
+        if val == value:
+            keys_to_delete.append(key)
+    for key in keys_to_delete:
+        del a_dictionary[key]
