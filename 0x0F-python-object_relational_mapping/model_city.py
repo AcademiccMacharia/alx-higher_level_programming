e186120fe0c20b4aa2215792ec8f168dd02e5755
AcademiccMacharia <machariab19@gmail.com>
2023-05-03 13:07:34 +0300

    adding file with class def of City and another file that prints all City objects

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+New
+"""
+
+from sqlalchemy import MetaData, Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+class City(Base):
+    """
+    Class with id, name and state_id that links to cities
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, foreign_key('state.id'))
