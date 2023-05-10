b81c4885b41020227f5f867ab3ae7c9c6cef48de
Ben Macharia <machariab19@gmail.com>
2023-05-10 11:44:54 +0300

    Delete 0x12-JavaScript Warm Up directory

--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const lines = {
-  first: 'C is fun',
-  second: 'Python is cool',
-  third: 'JavaScript is amazing'
-};
-console.log(lines.first);
-console.log(lines.second);
-console.log(lines.third);
