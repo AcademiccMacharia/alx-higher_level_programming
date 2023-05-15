8cdf963150c53be508f727d94468f7d50ae81198
AcademiccMacharia <machariab19@gmail.com>
2023-05-15 15:45:28 +0300

    adding doctest file for 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,9 +9,9 @@ def add_integer(a, b=98):
     """
     Function that adds two ntegers
     """
-    if not isinstance(a, int) | | not isinstance(a, float):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) | | not isinstance(b, float):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
 
     a = int(a)
